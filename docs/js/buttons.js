5b62e84a4943249ab25bd52691ca56bfa7439469
markjt031 <markjt031@gmail.com>
2023-05-02 21:01:12 -0400

    Error message displays

--- docs/js/buttons.js
+++ docs/js/buttons.js
@@ -1,1 +1,14 @@
-let submitButton=document.getElementById()
+let submitButton=document.getElementById('submit-button');
+let email=document.getElementById('email');
+let error=document.getElementById('error');
+
+handleInvalid=(e)=>{
+    e.preventDefault();
+    error.hidden=false;
+}
+handleSubmit=(e)=>{
+    e.preventDefault();
+    error.hidden=false;
+}
+email.oninvalid=handleInvalid;
+submitButton.onsubmit=handleSubmit;
